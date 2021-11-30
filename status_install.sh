d6bc52b9a55930aef02ac8779826d81922d2587d
One <admin@zhouchunyu.com>
2021-11-30 23:38:39 +0800

    Init

--- status_install.sh
+++ status_install.sh
@@ -105,17 +105,17 @@ check_pid_client(){
 Start_ServerStatus_client(){
 	check_pid_client
 	[[ ! -z ${PID} ]] && echo -e "${Error} ServerStatusPlus 正在运行，请检查 !" && exit 1
-	/etc/init.d/status-plus-client start
+	/etc/init.d/status start
 }
 Stop_ServerStatus_client(){
 	check_pid_client
 	[[ -z ${PID} ]] && echo -e "${Error} ServerStatusPlus 没有运行，请检查 !" && exit 1
-	/etc/init.d/status-plus-client stop
+	/etc/init.d/status stop
 }
 Restart_ServerStatus_client(){
 	check_pid_client
-	[[ ! -z ${PID} ]] && /etc/init.d/status-plus-client stop
-	/etc/init.d/status-plus-client start
+	[[ ! -z ${PID} ]] && /etc/init.d/status stop
+	/etc/init.d/status start
 }
 Uninstall_ServerStatus_client(){
 	echo "确定要卸载 ServerStatusPlus 客户端(如果同时安装了服务端，则只会删除客户端) ? [y/N]"
@@ -126,11 +126,11 @@ Uninstall_ServerStatus_client(){
 		check_pid_client
 		[[ ! -z $PID ]] && kill -9 ${PID}
 		rm -rf "/usr/local/ServerStatusPlus"
-		rm -rf /etc/init.d/status-plus-client
+		rm -rf /etc/init.d/status
 		if [[ ${release} = "centos" ]]; then
-			chkconfig --del status-plus-client
+			chkconfig --del status
 		else
-			update-rc.d -f status-plus-client remove
+			update-rc.d -f status remove
 		fi
 		echo && echo "ServerStatusPlus 卸载完成 !" && echo
 	else

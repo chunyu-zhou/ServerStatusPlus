647eff07584ac4ae30904298b863e6db975f99f2
One <admin@zhouchunyu.com>
2022-10-09 15:02:19 +0800

    提交

--- status-plus-client.py
+++ status-plus-client.py
@@ -55,11 +55,11 @@ else:
 
 def request_fun(path='',data={},request_type='get'):
     request_type == request_type.lower()
-    APIURL = "{}{}?server_token={}&group_token={}&user_token={}".format(APIDOMAIN,path,SERVERTOKEN,GROUPTOKEN,USERTOKEN)
+    APIURL = "{}{}?server_token={}&client_version={}&ipv4={}&ipv6={}".format(APIDOMAIN,path,SERVERTOKEN,VERSION,IPV4,IPV6)
     if request_type == 'get':
-        res = requests.get(APIURL, data=data,headers={"server_token":SERVERTOKEN,"client_version":VERSION}, timeout=30, verify=False)
+        res = requests.get(APIURL, data=data,headers={"server_token":SERVERTOKEN}, timeout=30, verify=False)
     elif request_type == 'post':
-        res = requests.post(APIURL, data=data,headers={"server_token":SERVERTOKEN,"client_version":VERSION}, timeout=30, verify=False)
+        res = requests.post(APIURL, data=data,headers={"server_token":SERVERTOKEN}, timeout=30, verify=False)
     return res
 
 # 参考文档 https://blog.csdn.net/qq_26373925/article/details/108047836
